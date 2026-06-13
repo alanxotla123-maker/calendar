763eb8bd3a6f48bdcbfb7e200467bf010fa7dfb3
alanxotla123 <alanxotla123@gmail.com>
2026-06-13 13:16:08 -0600

    organizacion de carpetas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
+import { TasksModule } from './tasks/tasks.module';
+import { SavingsModule } from './savings/savings.module';
 
 @Module({
-  imports: [],
+  imports: [CategoriesModule, TasksModule, SavingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
